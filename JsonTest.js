14762e1e4332d3893cfe972fdfaab2fbd08bf098
jmyoon8 <jmyoon8@naver.com>
2020-09-22 23:22:19 +0900

    챗팅

---
+++ JsonTest.js
@@ -0,0 +1,25 @@
+const fs= require('fs')
+
+//JSON 데이터 불러오기 어레이값을 불러온다
+var data=fs.readFileSync("./json/vocab.json","utf8")
+//그값을 JSON.parse를 통해 OBJECT화 한다.
+let a=JSON.parse(data)
+//인덱스 값으로 값을 불러온다.
+console.log(a[0])
+//JSON.parse를 하지않으면 String 그대로 가저온다.(문자그대로 인식한다.)
+
+
+//JSON 데이터 저장하기(file로) 데이터를 저장 할때는 String 화 시켜야한다.
+const test ={
+    name:"jmy",
+    age:20,
+    description:"i am i'm"
+}
+fs.writeFileSync("./json/test.json",JSON.stringify(test, null , 2));
+                                                        //,null ,2를하면 이쁘게 저장한다.
+                                                        
+//json 파일을 불러올땐 parse로 object화
+//json 파일을 저잘할땐 Stringify로 String화하여 저장해야한다.
+
+const express =require('express');
+const app=express();
--- express.js
+++ express.js
@@ -1,40 +1,65 @@
-const express =require('express');
-const server=express();
+const express = require('express')
+const app = express();
 
+//static(정적) 경로 등록방법
+//use란 서버에 오는 모든 명령어는 use에 등록된 정보를 거쳐서 가야한다는 의미로 middleware라고도 한다.
+//만약 /css 라는 명령이 들어오면 다음과같이 static 경로로 변환하라 라는뜻 
+app.use('/css',express.static(__dirname+'/css'))
+app.use('/image',express.static(__dirname+'/image'))
+app.use('/javascript',express.static(__dirname+'/javascript'))
 
-server.use('/css',express.static(__dirname+'/css'))//css파일의 경로를 지정해두어 html에 css가 적용되도록한다.
-server.use('/image',express.static(__dirname+'/image'))
-server.use('/javascript',express.static(__dirname+'/javascript'))
+//function(req,res,next){}를 통해 req, res를 컨트롤 할 수도 있다.
+app.use(function(req,res,next){
+    req.user={
+        id:"1234"
+    }
+    //이후 모든 라우터에서 console.log(res.user)  을 찍어보면 값을 반환한다.
+    //미들 웨어가 실행되고 next를 통해 다음 을 실행하라고 명령한다.
+    next();
+})
 
 //서버로의 요청이 get방식일때
-server.get("/",function(req,res){
+
+app.get("/doctor",function(req,res){
     
+    console.log(req.user['id'])
     
-    res.sendFile(__dirname+"/index.html")
+    res.sendFile(__dirname+"/client/doctor.html")
     
 })
 
-server.get("/d",function(req,res){
+app.get("/guest",function(req,res){
     
     
-    res.sendFile(__dirname+"/template.html")
+    res.sendFile(__dirname+"/client/guest.html")
+    
+})
+
+
+app.get("/d",function(req,res){
+    
+    
+    res.sendFile(__dirname+"/referenceTemplate/template.html")
     
 })
 
 //서버로의 요청이 post방식일때
-server.post("",function(req,res){
+app.post("",function(req,res){
 
 })
 
 //어떤 값이 delete될떄
-server.delete("")
+app.delete("")
 
 //어떤 값이 put될때
-server.put("")
+app.put("")
 
+//페이지를 찾지 못했을때
+app.use((req,res)=>{
+    res.sendFile(__dirname+"/client/404.html")
+})
 
-
-server.listen(2000,function(err){
+app.listen(2000,function(err){
     if(err) return console.log(err)
 
     console.log("the sever is listening on 3000")
